517e757362b2b5957262f35970daf7483ce6c0cd
Kunal Pardeshi <kunalpardeshi4042@gmail.com>
2021-02-08 22:41:25 +0530

    1st commit

---
+++ aboutus/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from .models import Aboutus,Why_Choose_Us,Chef
+
+# Register your models here.
+
+admin.site.register(Aboutus)
+admin.site.register(Why_Choose_Us)
+admin.site.register(Chef)
