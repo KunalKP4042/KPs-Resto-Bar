858741c021dc639c604d87cf3aa7e45abf1da2f3
Kunal Pardeshi <kunalpardeshi4042@gmail.com>
2021-04-18 11:43:17 +0530

    Final Commit

--- restaurant/settings.py
+++ restaurant/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 import email
+import os
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -26,9 +27,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'cq(m_=xvl$2^x813$i)3ghrr*h*4tz*9ji!_w0)2!@@xu+=b4n'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['https://kpsrestonbar.herokuapp.com/','localhost']
 
 
 # Application definition
@@ -53,6 +54,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -128,6 +130,8 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
+
+SECRET_KEY = os.environ['SECRET_KEY']
 
 STATIC_URL = '/static/'
 STATIC_URLS = BASE_DIR/'static'
